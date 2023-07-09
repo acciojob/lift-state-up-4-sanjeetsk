c83b276d59f4f2ef6d7182624954130bb750f30b
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-07-09 17:23:14 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ const App = () => {
         <input type="text" id="itemName" placeholder="Item Name"/>
         <label htmlFor="itemPrice">Item Price</label>
         <input type="number" id="itemPrice" placeholder="Item Price"/>
-        <button onClick={handleAddItems}>Add items</button>
+        <button onClick={handleAddItems}>Add Item</button>
       </div>
       <ChildComponent cartItems={cartItems} handleRemoveItem={handleRemoveItem} />
     </div>
@@ -46,7 +46,7 @@ const ChildComponent = ({ cartItems, handleRemoveItem }) => {
   return (
     <div className="child">
       <h2>Child Component</h2>
-      <ul>
+      <>
         {cartItems.map((item, index) => (
           <li key={index}>
             <span id="itemName">{item.name}</span>&nbsp;&nbsp;
@@ -54,7 +54,7 @@ const ChildComponent = ({ cartItems, handleRemoveItem }) => {
             <button onClick={() => handleRemoveItem(item.name)}>Remove</button>
           </li>
         ))}
-      </ul>
+      </>
     </div>
   );
 };
